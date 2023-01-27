7e3c940f144d5ebbdf82e30b7621a815874ed9f2
DanK04 <kawazudan0430@gmail.com>
2023-01-27 17:35:21 +0900

    チャンネル登録機能追加

--- app.py
+++ app.py
@@ -14,11 +14,45 @@ def index():
         return redirect('/login')
 
     #uidが空でない＝セッション情報の中にuid情報が登録されている＝ログイン済みの場合
-    else:   
+    else:
         #データベースからチャンネル情報を取得して変数channelsへ代入
         channels = dbConnect.getChannelAll()
     #ホーム画面のWEBページを返す。HTMLファイルで使う変数は引数で渡す。
-    return render_template('index.html', channels=channels, uid=uid)
+    return render_template('index.html', channels=channels) # 一時的にuid=uidを消してる
+
+@app.route('/', methods=['POST'])
+def add_channel():
+    """ ユーザーID
+
+    ユーザーIDをセッションから取得してuidに代入
+    ユーザーIDが無ければログインページへリダイレクト
+    """
+    #! ユーザー機能作成までコメントアウト
+    # uid = session.get('uid')
+    # if uid is None:
+    #     return redirect('/login')
+
+    """チャンネル名
+
+    フォームからチャンネル名を取得→channel_nameへ代入
+    データベースから、入力されたチャンネル名と同じチャンネルを取得→channelへ代入
+
+    if チャンネル名がデータベースに存在しない場合:
+        チャンネル説明文をフォームから取得→channel_descriptionへ代入
+        データベースに(ユーザーID, チャンネル名, チャンネル説明文)を追加
+        チャンネル一覧表示画面へリダイレクト
+    else: (チャンネル名がデータベースに存在した場合)
+        エラーページを表示
+    """
+    channel_name = request.form.get('channel-title')
+    channel = dbConnect.getChannelByName(channel_name)
+    if channel == None:
+        channel_description = request.form.get('channel-description')
+        dbConnect.addChannel(channel_name, channel_description) #! uidを追加
+        return redirect('/')
+    else:
+        error = '既に同じチャンネルが存在しています'
+        return render_template('error/error.html', error_message=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import pymysql
-from util.DB import DB 
+from util.DB import DB
 
 class dbConnect:
 #チャンネル一覧取得機能
@@ -16,11 +16,70 @@ class dbConnect:
             channels = cur.fetchall()
             return channels
 
-        #例外処理　
+        #例外処理
         except Exception as e:
             print(e + 'が発生しています')
             return None
-        
-        #最終処理　カーソルを閉じる
+
+        #最終処理 カーソルを閉じる
+        finally:
+            cur.close()
+
+    # チャンネル名取得
+    def getChannelByName(channel_name):
+        """
+        MySQLにDBクラスで定義した接続用メソッドを使用して接続
+        カーソルを作成→curへ代入
+        * カーソルとは - 検索結果からデータを１件ずつ取得する仕組みまたその目印のこと
+        channelsテーブルから該当のテーブル名を取得する処理するりそのもの→sqlへ代入
+        channelsテーブルから引数で渡ってきたchannel_nameという値を検索し、抜き取る
+        * executeとは - SQL文を渡すと実行してくれる関数
+        fetchoneでchannel名が一致する1行のみデータを取得→channelに代入
+        channelを返す
+        """
+
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = 'SELECT * FROM channels WHERE name=%s;'
+            cur.execute(sql, (channel_name))
+            channel = cur.fetchone()
+            return channel
+
+        # 例外処理
+        except Exception as e:
+            print(e + 'が発生しています')
+            return None
+
+        # 最終処理:カーソルを閉じる
+        finally:
+            cur.close()
+
+    # チャンネル追加(ユーザーID, チャンネル名, チャンネル概要)
+    def addChannel(newChannelName, newChannelDescription): #! uidを追加
+        """
+        MySQLにDBクラスで定義した接続用メソッドを使用して接続
+        カーソルを作成→curへ代入
+        sqlにSQL文を代入
+        execute分でsqlを実行(channelテーブルにユーザーID,チャンネル名,チャンネル概要を追加)
+        commitで変更を確定
+        * commitとは - トランザクションの結果を確定する(変更を確定する)
+        """
+
+        try:
+            #! 仮uid
+            uid = '970af84c-dd40-47ff-af23-282b72b7cca8'
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = 'INSERT INTO channels (uid, name, abstract) VALUES (%s, %s, %s);'
+            cur.execute(sql, (uid, newChannelName, newChannelDescription))
+            conn.commit()
+
+        # 例外処理
+        except Exception as e:
+            print(e + 'が発生しています')
+            return None
+
+        # 最終処理:カーソルを閉じる
         finally:
             cur.close()
