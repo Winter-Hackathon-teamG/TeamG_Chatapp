f151b96304c63f974f0c614b70f27d4f20be4d7c
knks <skaneko449@gmail.com>
2023-02-27 22:18:27 +0900

    delete modal complete

--- static/js/delete-channel.js
+++ static/js/delete-channel.js
@@ -15,3 +15,8 @@ deleteButton.addEventListener("click", deleteChannel);
 deletePageButtonClose.addEventListener("click", () => {
   modalClose("delete");
 });
+
+// 削除確認モーダルでの削除実行コード
+const confirmationButtonLink = document.getElementById("delete-confirm-link"); // id取得（aタグ）
+const url = `/delete/${channel.id}`; // urlを作成
+confirmationButtonLink.setAttribute("href", url); // urlを取得id（aタグ）のhref属性に設定
