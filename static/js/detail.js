9ddc5d384ecbd37e1d9897d87842e2ef00351bed
knks <skaneko449@gmail.com>
2023-03-01 00:38:57 +0900

    chat finish

--- static/js/detail.js
+++ static/js/detail.js
@@ -7,7 +7,7 @@ function calculateHeaderHeight() {
   const tagCount = chatHeader.childElementCount; // タグの表示数取得
   const headerWidth = chatHeader.offsetWidth; // ヘッダーの幅取得
   const tagWidth = chatHeader.firstElementChild.offsetWidth; // タグの幅取得
-  const tagHeight = 120; // 一行あたりのタグ表示の高さを設定
+  const tagHeight = 40; // 一行あたりのタグ表示の高さを設定
   const maxTagPerRow = Math.floor(headerWidth / tagWidth); // 各行に表示できるタグの最大数
   const rowCount = Math.ceil(tagCount / maxTagPerRow); // タグ表示の行数
   const headerHeight = rowCount * tagHeight; // ヘッダー高さ
